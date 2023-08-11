a180f90a5ed6d20f7e42ccf2bbbe55f8a1f774c1
MagdalenaPizarroO <mspizarro.ortega@gmail.com>
2023-08-10 23:13:07 -0400

    working on Modals

--- README.md
+++ README.md
@@ -36,3 +36,13 @@ You can check out [the Next.js GitHub repository](https://github.com/vercel/next
 The easiest way to deploy your Next.js app is to use the [Vercel Platform](https://vercel.com/new?utm_medium=default-template&filter=next.js&utm_source=create-next-app&utm_campaign=create-next-app-readme) from the creators of Next.js.
 
 Check out our [Next.js deployment documentation](https://nextjs.org/docs/deployment) for more details.
+
+----
+<!-- 
+Iconos: npm install react-icons https://react-icons.github.io/react-icons
+Global states for the navbar: npm install recoil    https://recoiljs.org/docs/introduction/getting-started
+
+
+
+
+ -->
--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,12 +1,18 @@
+import { authModalState } from '@/atoms/authModalAtom';
 import Link from 'next/link';
 import React from 'react';
+import { useSetRecoilState } from 'recoil';
 
 type NavbarProps = {
     
 };
 
 const Navbar:React.FC<NavbarProps> = () => {
-    
+    const setAuthModalState=useSetRecoilState(authModalState)
+    const handleClick = () => {
+        setAuthModalState((prev)=> ({...prev, isOpen: true}));
+    }
+
     return <div className='flex items-center justify-between sm:px-12 px-2 md:px-24'>
         <Link href="/" className='flex items-center justify-center h-20'>
             <img src="/logo.png" alt="LeetClone" className='h-full' />
@@ -14,7 +20,9 @@ const Navbar:React.FC<NavbarProps> = () => {
         <div className='flex items-center'>
             <button className='bg-brand-orange text-white px-2 py-1 sm:px-2 rounded-md text-sm font-medium border-2 border-transparent
             hover:text-brand-orange hover:bg-white hover:border-2 hover:border-brand-orange
-            transition duration-300 ease-in-out'>
+            transition duration-300 ease-in-out'
+            onClick={handleClick}
+            >
                 Sign In
             </button>
         </div>
