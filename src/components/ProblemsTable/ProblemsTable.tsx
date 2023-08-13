008da8b3db152f9b5fbb050930aa94268f2a0ea9
MagdalenaPizarroO <mspizarro.ortega@gmail.com>
2023-08-12 23:40:46 -0400

    problems page design ok

--- src/components/ProblemsTable/ProblemsTable.tsx
+++ src/components/ProblemsTable/ProblemsTable.tsx
@@ -37,24 +37,16 @@ const ProblemsTable: React.FC<ProblemsTableProps> = () => {
               ? "text-dark-yellow"
               : "text-dark-pink";
           return (
-            <tr
-              className={`${idx % 2 == 1 ? "bg-dark-layer-1" : ""}`}
-              key={doc.id}
-            >
+            <tr className={`${idx % 2 == 1 ? "bg-dark-layer-1" : ""}`} key={doc.id}>
               <th className="px-2 py-4 font-medium whitespace-nowrap text-dark-green-s">
                 <BsCheckCircle fontSize={"18"} width="18" />
               </th>
               <td className="px-6 py-4">
-                <Link
-                  href={`/problems/${doc.id}`}
-                  className="hover:text-blue-600 cursor-pointer"
-                >
+                <Link href={`/problems/${doc.id}`} className="hover:text-blue-600 cursor-pointer">
                   {doc.title}
                 </Link>
               </td>
-              <td className={`px-6 py-4 ${difficultyColor}`}>
-                {doc.difficulty}
-              </td>
+              <td className={`px-6 py-4 ${difficultyColor}`}>{doc.difficulty}</td>
               <td className={"px-6 py-4"}>{doc.category}</td>
               <td className={"px-6 py-4"}>
                 {doc.videoId ? (
@@ -78,23 +70,12 @@ const ProblemsTable: React.FC<ProblemsTableProps> = () => {
       </tbody>
       {youtubePlayer.isOpen && (
         <tfoot className="fixed top-0 left-0 h-screen w-screen flex items-center justify-center ">
-          <div
-            className="bg-black z-10 opacity-70 top-0 left-0 w-screen h-screen absolute"
-            onClick={closeModal}
-          ></div>
+          <div className="bg-black z-10 opacity-70 top-0 left-0 w-screen h-screen absolute" onClick={closeModal}></div>
           <div className="w-full z-50 h-full px-6 relative max-w-4xl">
             <div className="w-full h-full flex items-center justify-center relative">
               <div className="w-full relative">
-                <IoClose
-                  fontSize={"35"}
-                  className="cursor-pointer absolute -top-16 right-0"
-                  onClick={closeModal}
-                />
-                <YouTube
-                  videoId={youtubePlayer.videoId}
-                  loading="lazy"
-                  iframeClassName="w-full min-h-[500px]"
-                />
+                <IoClose fontSize={"35"} className="cursor-pointer absolute -top-16 right-0" onClick={closeModal} />
+                <YouTube videoId={youtubePlayer.videoId} loading="lazy" iframeClassName="w-full min-h-[500px]" />
               </div>
             </div>
           </div>
