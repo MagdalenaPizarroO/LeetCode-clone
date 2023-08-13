b97fd44ce4fe374be0ba346b963b9be7b39063bf
MagdalenaPizarroO <mspizarro.ortega@gmail.com>
2023-08-12 21:30:40 -0400

    image optimization

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,32 +1,33 @@
-import { authModalState } from '@/atoms/authModalAtom';
-import Link from 'next/link';
-import React from 'react';
-import { useSetRecoilState } from 'recoil';
+import { authModalState } from "@/atoms/authModalAtom";
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+import { useSetRecoilState } from "recoil";
 
-type NavbarProps = {
-    
-};
+type NavbarProps = {};
 
-const Navbar:React.FC<NavbarProps> = () => {
-    const setAuthModalState=useSetRecoilState(authModalState)
-    const handleClick = () => {
-        setAuthModalState((prev)=> ({...prev, isOpen: true}));
-    }
+const Navbar: React.FC<NavbarProps> = () => {
+  const setAuthModalState = useSetRecoilState(authModalState);
+  const handleClick = () => {
+    setAuthModalState((prev) => ({ ...prev, isOpen: true }));
+  };
 
-    return <div className='flex items-center justify-between sm:px-12 px-2 md:px-24'>
-        <Link href="/" className='flex items-center justify-center h-20'>
-            <img src="/logo.png" alt="LeetClone" className='h-full' />
-        </Link>
-        <div className='flex items-center'>
-            <button className='bg-brand-orange text-white px-2 py-1 sm:px-2 rounded-md text-sm font-medium border-2 border-transparent
+  return (
+    <div className="flex items-center justify-between sm:px-12 px-2 md:px-24">
+      <Link href="/" className="flex items-center justify-center h-20">
+        <Image src="/logo.png" alt="LeetClone" height={200} width={200} />
+      </Link>
+      <div className="flex items-center">
+        <button
+          className="bg-brand-orange text-white px-2 py-1 sm:px-2 rounded-md text-sm font-medium border-2 border-transparent
             hover:text-brand-orange hover:bg-white hover:border-2 hover:border-brand-orange
-            transition duration-300 ease-in-out'
-            onClick={handleClick}
-            >
-                Sign In
-            </button>
-        </div>
-        
+            transition duration-300 ease-in-out"
+          onClick={handleClick}
+        >
+          Sign In
+        </button>
+      </div>
     </div>
-}
+  );
+};
 export default Navbar;
--- src/components/Topbar/Topbar.tsx
+++ src/components/Topbar/Topbar.tsx
@@ -5,6 +5,7 @@ import { useAuthState } from "react-firebase-hooks/auth";
 import Logout from "../Buttons/Logout";
 import { useSetRecoilState } from "recoil";
 import { authModalState } from "@/atoms/authModalAtom";
+import Image from "next/image";
 
 type TopbarProps = {};
 
@@ -14,11 +15,9 @@ const Topbar: React.FC<TopbarProps> = () => {
 
   return (
     <nav className="relative flex h-[50px] w-full shrink-0 items-center px-5 bg-dark-layer-1 text-dark-gray-7">
-      <div
-        className={`flex w-full items-center justify-between max-w-[1200px] mx-auto`}
-      >
+      <div className={`flex w-full items-center justify-between max-w-[1200px] mx-auto`}>
         <Link href="/" className="h-[22px] flex-1">
-          <img src="/logo-full.png" alt="Logo" className="h-full" />
+          <Image src="/logo-full.png" alt="Logo" height={100} width={100} />
         </Link>
 
         <div className="flex items-center space-x-4 flex-1 justify-end">
@@ -43,18 +42,12 @@ const Topbar: React.FC<TopbarProps> = () => {
                 }));
               }}
             >
-              <button className="bg-dark-fill-3 py-1 px-2 cursor-pointer rounded ">
-                Sign In
-              </button>
+              <button className="bg-dark-fill-3 py-1 px-2 cursor-pointer rounded ">Sign In</button>
             </Link>
           )}
           {user && (
             <div className="cursor-pointer group relative">
-              <img
-                src="/avatar.png"
-                alt="user profile img"
-                className="h-8 w-8 rounded-full"
-              />
+              <Image src="/avatar.png" alt="Avatar" width={30} height={30} className="rounded-full" />
               <div
                 className="absolute top-10 left-2/4 -translate-x-2/4  mx-auto bg-dark-layer-1 text-brand-orange p-2 rounded shadow-lg z-40 group-hover:scale-100 scale-0 
 			transition-all duration-300 ease-in-out"
